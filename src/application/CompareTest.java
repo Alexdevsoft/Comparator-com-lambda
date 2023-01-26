ba8f2e873ed2b312016e718c498d105337fdc58c
Alexdevsoft <alexhavilla2022@gmail.com>
2023-01-26 11:31:31 -0300

    Comparação com lambda

---
+++ src/application/CompareTest.java
@@ -0,0 +1,30 @@
+package application;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+import entities.Product;
+
+public class CompareTest {
+
+	public static void main(String[] args) {
+		
+		List<Product> list = new ArrayList<>();
+		
+		list.add(new Product("TV", 900.00));
+		list.add(new Product("Smartphone", 2000.00));
+		list.add(new Product("Fogão", 400.00));
+		
+		list.sort((p1, p2) -> p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase()));
+	
+		
+		
+		for(Product p : list) {
+			System.out.println(p);
+		}
+		
+
+	}
+
+}
